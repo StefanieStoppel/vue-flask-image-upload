a77a795b011482e5c5012dd637d0b3b72a5727a2
Stefanie Stoppel <sstoppel@inovex.de>
2020-07-02 16:02:51 +0200

    feat: include yarn build before starting server

--- start-app.sh
+++ start-app.sh
@@ -2,4 +2,9 @@
 
 UPLOAD_PATH="${1:-/Users/sstoppel/Desktop/vue-flask-image-upload}"
 
+# cd into client and run yarn build
+cd ./client || { echo "cd failed"; exit; }
+yarn build
+cd ..
+
 python server/app.py --upload_path "$UPLOAD_PATH"
